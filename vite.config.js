0f8df06fc21fa8d3d73a90b858f0d88ae44feaa9
Gabriel Fonseca <gabrielrf19@gmail.com>
2026-07-31 23:15:19 -0300

    Configuracao de build de producao Vite para Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        travel: resolve(__dirname, 'projeto-travel.html'),
+        megavarejo: resolve(__dirname, 'projeto-megavarejo.html'),
+        spark: resolve(__dirname, 'projeto-spark.html'),
+        saferout: resolve(__dirname, 'projeto-saferout.html')
+      }
+    }
+  },
   server: {
-    port: 3000,
-    open: true
+    port: 3000
   }
 })
